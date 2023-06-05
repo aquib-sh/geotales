b925783eb432e23e819c78df0e3a9efbae30767d
Aquib <aquib-shaikh@outlook.com>
2023-06-05 12:52:29 +0530

    Initial commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+    );
+
+    _animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: 1 + _animation.value * 0.2,
+                  child: child,
+                );
+              },
+              child: Icon(
+                Icons.public,
+                size: 100,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'GeoTales',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 40),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
